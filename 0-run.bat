252edcab0957aaafa888fcef7ddcd4c302be52c9
米鸿坤 <1045262335@qq.com>
2022-04-12 11:28:35 +0100

    feat: charge debug

--- 0-run.bat
+++ 0-run.bat
@@ -3,6 +3,10 @@
 call rebar3 as prod release
 cd _build\prod\rel\ml_charge
 bin\ml_charge-0.1.0.cmd console
-
 cd ..\..\..\..
 
+
+@REM call rebar3 release
+@REM cd _build\default\rel\ml_charge
+@REM bin\ml_charge-0.1.0.cmd console
+@REM cd ..\..\..\..
